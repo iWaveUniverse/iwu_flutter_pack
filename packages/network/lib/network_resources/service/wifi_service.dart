46195779a2faa2715ab92b517669dc43f3849973
Minh Minh <mminh.lequang@gmail.com>
2024-03-27 09:18:15 +0700

    -

--- packages/network/lib/network_resources/service/wifi_service.dart
+++ packages/network/lib/network_resources/service/wifi_service.dart
@@ -5,22 +5,22 @@ import 'package:connectivity_plus/connectivity_plus.dart';
 typedef WifiListener = Function(bool enabled);
 
 class WifiService {
-  StreamSubscription<ConnectivityResult>? _subscription;
+  StreamSubscription<List<ConnectivityResult>>? _subscription;
 
-  static ConnectivityResult? _connectivityResult;
+  static List<ConnectivityResult>? _connectivityResult;
 
-  static Stream<ConnectivityResult> get onConnectivityChanged =>
+  static Stream<List<ConnectivityResult>> get onConnectivityChanged =>
       Connectivity().onConnectivityChanged;
 
   WifiService({WifiListener? listener}) {
     if (_subscription != null) _subscription!.cancel();
     _subscription = Connectivity()
         .onConnectivityChanged
-        .listen((ConnectivityResult result) {
-      _connectivityResult = result;
+        .listen((List<ConnectivityResult> results) {
+      _connectivityResult = results;
       // Got a new connectivity status!
       if (listener != null) {
-        listener(result != ConnectivityResult.none);
+        listener(isConnected(results));
       }
     });
   }
@@ -28,23 +28,55 @@ class WifiService {
   static Future<bool> isConnectivity() async {
     var connectivityResult =
         _connectivityResult ?? await (Connectivity().checkConnectivity());
-    return connectivityResult != ConnectivityResult.none;
+    return isConnected(connectivityResult);
   }
 
   static Future<bool> isDisconnectWhenCallApi() async {
     var connectivityResult =
         _connectivityResult ?? await Connectivity().checkConnectivity();
-    bool status = connectivityResult == ConnectivityResult.none;
-    return status;
+    bool status = isConnected(connectivityResult);
+    return !status;
   }
 
   static Future<bool> isDisconnect() async {
     var connectivityResult =
         _connectivityResult ?? await Connectivity().checkConnectivity();
-    return connectivityResult == ConnectivityResult.none;
+    return !isConnected(connectivityResult);
   }
 
   close() {
     _subscription?.cancel();
+  }
+
+  static bool isConnected(List<ConnectivityResult> connectivityResult) {
+    bool value = false;
+    if (connectivityResult.contains(ConnectivityResult.mobile)) {
+      // Mobile network available.
+      value = true;
+    } else if (connectivityResult.contains(ConnectivityResult.wifi)) {
+      // Wi-fi is available.
+      // Note for Android:
+      // When both mobile and Wi-Fi are turned on system will return Wi-Fi only as active network type
+      value = true;
+    } else if (connectivityResult.contains(ConnectivityResult.ethernet)) {
+      // Ethernet connection available.
+      value = true;
+    } else if (connectivityResult.contains(ConnectivityResult.vpn)) {
+      // Vpn connection active.
+      // Note for iOS and macOS:
+      // There is no separate network interface type for [vpn].
+      // It returns [other] on any device (also simulator)
+      value = true;
+    } else if (connectivityResult.contains(ConnectivityResult.bluetooth)) {
+      // Bluetooth connection available.
+      value = true;
+    } else if (connectivityResult.contains(ConnectivityResult.other)) {
+      // Connected to a network which is not in the above mentioned networks.
+      value = true;
+    } else if (connectivityResult.contains(ConnectivityResult.none)) {
+      // No available network types
+      value = false;
+    }
+    return value;
   }
 }
